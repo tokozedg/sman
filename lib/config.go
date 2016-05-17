bd060c0eddd0b4d28d5ef100fd536d6e6ff538fc
Tornike Zedginidze <tokozedg@gmail.com>
2016-05-17 22:14:01 +0400

    change default values for config parameters to be set by viper

--- lib/config.go
+++ lib/config.go
@@ -10,10 +10,16 @@ type Config struct {
 	ExecConfirm, AppendHistory bool
 }
 
+func init() {
+	viper.SetDefault("snippet_dir", "~/snippets")
+	viper.SetDefault("append_history", "true")
+	viper.SetDefault("exec_confirm", "true")
+}
+
 //GetConfig reads and return config struct
 func GetConfig() (c Config) {
 	c.SnippetDir = ExpandPath(viper.GetString("snippet_dir"))
-	c.Shell = ExpandPath(viper.GetString("shell"))
+	c.Shell = viper.GetString("shell")
 	c.AppendHistory = viper.GetBool("append_history")
 	c.ExecConfirm = viper.GetBool("exec_confirm")
 	return c
